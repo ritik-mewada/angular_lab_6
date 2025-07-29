d19fe7007a18c1099749c028e3fabaaa4f4fef75
ritik-mewada <ritikmewada@gmail.com>
2025-07-29 14:53:24 -0400

    updated changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { Task } from './models/task';
 import { FooterComponent } from './shared/footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
   imports: [HeaderComponent, FooterComponent, RouterModule],
 })
 export class AppComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
