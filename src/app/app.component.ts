973983e4e3ad2f4be1aa7060050e540f4d65365c
ritik-mewada <ritikmewada@gmail.com>
2025-07-29 14:32:28 -0400

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,4 @@ import { HeaderComponent } from './shared/header/header.component';
 })
 export class AppComponent {
   title = 'Task Manager';
-
-  tasks: Task[] = [];
-
-  addTask(newTask: Omit<Task, 'id' | 'createdDate'>): void {
-    const task: Task = {
-      ...newTask,
-      id: Math.max(...this.tasks.map((t) => t.id), 0) + 1,
-      createdDate: new Date(),
-    };
-    this.tasks.push(task);
-  }
-
-  deleteTask(taskId: number): void {
-    this.tasks = this.tasks.filter((task) => task.id !== taskId);
-  }
-
-  toggleTask(taskId: number): void {
-    const task = this.tasks.find((t) => t.id === taskId);
-    if (task) {
-      task.completed = !task.completed;
-    }
-  }
 }
